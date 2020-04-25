d0e182b552f40775e910088d864124ddda7b86fe
vitebo <vitebo@hotmail.com>
2020-04-25 18:47:51 -0300

    feat(dashboard): create page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,64 +1,62 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import * as S from './styled';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <S.Title>Explore Repositórios no Github</S.Title>
-    <S.Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </S.Form>
-    <S.Repositories>
-      <a href="/">
-        <img
-          src="https://avatars0.githubusercontent.com/u/19560693?s=460&u=3ec5a2586a893123703ffacf991e459da8dc9954&v=4"
-          alt="André Alves Vitebo"
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <S.Title>Explore Repositórios no Github</S.Title>
+      <S.Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
         />
-        <div>
-          <strong>vitebo/dotfiles</strong>
-          <p>:wrench: Building my dotfiles</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="/">
-        <img
-          src="https://avatars0.githubusercontent.com/u/19560693?s=460&u=3ec5a2586a893123703ffacf991e459da8dc9954&v=4"
-          alt="André Alves Vitebo"
-        />
-        <div>
-          <strong>vitebo/dotfiles</strong>
-          <p>:wrench: Building my dotfiles</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="/">
-        <img
-          src="https://avatars0.githubusercontent.com/u/19560693?s=460&u=3ec5a2586a893123703ffacf991e459da8dc9954&v=4"
-          alt="André Alves Vitebo"
-        />
-        <div>
-          <strong>vitebo/dotfiles</strong>
-          <p>:wrench: Building my dotfiles</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="/">
-        <img
-          src="https://avatars0.githubusercontent.com/u/19560693?s=460&u=3ec5a2586a893123703ffacf991e459da8dc9954&v=4"
-          alt="André Alves Vitebo"
-        />
-        <div>
-          <strong>vitebo/dotfiles</strong>
-          <p>:wrench: Building my dotfiles</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-    </S.Repositories>
-  </>
-);
+        <button type="submit">Pesquisar</button>
+      </S.Form>
+      <S.Repositories>
+        {repositories.map((repository) => (
+          <a href="/" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </S.Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/pages/Dashboard/styled.ts
+++ src/pages/Dashboard/styled.ts
@@ -4,23 +4,23 @@ import { shade } from 'polished';
 export const Title = styled.h1`
   color: #3a3a3a;
   font-size: 48px;
+  line-height: 56px;
   margin-top: 80px;
   max-width: 450px;
-  line-height: 56px;
 `;
 
 export const Form = styled.form`
+  display: flex;
   margin-top: 40px;
   max-width: 700px;
-  display: flex;
 
   input {
+    border-radius: 5px 0 0 5px;
+    border: 0;
+    color: #3a3a3a;
     flex: 1;
     height: 70px;
     padding: 0 24px;
-    border: 0;
-    border-radius: 5px 0 0 5px;
-    color: #3a3a3a;
 
     &::placeholder {
       color: #a8a8b3;
@@ -30,12 +30,12 @@ export const Form = styled.form`
   button {
     background-color: #04d361;
     border-radius: 0 5px 5px 0;
-    width: 210px;
-    height: 70px;
     border: none;
     color: #fff;
     font-weight: bold;
+    height: 70px;
     transition: background-color 250ms;
+    width: 210px;
 
     &:hover,
     &:focus {
@@ -49,15 +49,15 @@ export const Repositories = styled.div`
   max-width: 700px;
 
   a {
+    align-items: center;
     background-color: #fff;
     border-radius: 5px;
-    width: 100%;
-    padding: 24px;
     display: block;
-    text-decoration: none;
     display: flex;
-    align-items: center;
+    padding: 24px;
+    text-decoration: none;
     transition: transform 250ms;
+    width: 100%;
 
     &:focus,
     &:hover {
@@ -69,29 +69,30 @@ export const Repositories = styled.div`
     }
 
     img {
-      width: 64px;
-      height: 64px;
       border-radius: 50%;
+      height: 64px;
+      width: 64px;
     }
 
     div {
-      margin-left: 16px;
+      flex: 1;
+      margin: 0 16px;
 
       strong {
-        font-size: 20px;
         color: #3d3d4d;
+        font-size: 20px;
       }
 
       p {
-        font-size: 18px;
         color: #a8a8b3;
+        font-size: 18px;
         margin-top: 4px;
       }
     }
 
     svg {
-      margin-left: auto;
       color: #cbcbd6;
+      margin-left: auto;
     }
   }
 `;
