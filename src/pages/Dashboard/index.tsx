8ddb71f9f6810b211b14c27249c33b61f6742a31
vitebo <vitebo@hotmail.com>
2020-04-25 17:23:36 -0300

    feat(app): creating a basic structure

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styled';
+
+const Dashboard: React.FC = () => {
+  return <S.Title>Explore Repositórios no Github</S.Title>;
+};
+
+export default Dashboard;
