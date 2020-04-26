e03ef32360fadda69f38d2c1ab29a2f4979e562f
vitebo <vitebo@hotmail.com>
2020-04-25 23:19:32 -0300

    feat(repository): create page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import * as S from './styled';
+import api from '../../services/api';
 
 interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  open_issues_count: number;
+  forks_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  title: string;
+  id: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then((response) => {
+      setRepository(response.data);
+    });
+    api.get(`repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+  }, [params.repository]);
 
   return (
     <>
@@ -21,40 +55,51 @@ const Repository: React.FC = () => {
           Voltar
         </Link>
       </S.Header>
-      <S.RepositoryInfo>
-        <header>
-          <img
-            src="https://avatars0.githubusercontent.com/u/19560693?s=460&u=3ec5a2586a893123703ffacf991e459da8dc9954&v=4"
-            alt="André Alves Vitebo"
-          />
-          <div>
-            <strong>vitebo/dotfiles</strong>
-            <p>:wrench: Building my dotfiles</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>1808</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </S.RepositoryInfo>
+      {repository ? (
+        <S.RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </S.RepositoryInfo>
+      ) : (
+        <p>Carregando</p>
+      )}
       <S.Issues>
-        <Link to="/">
-          <div>
-            <strong>Teste</strong>
-            <p>Teste</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
+        {issues.map((issue) => (
+          <a
+            href={issue.html_url}
+            key={issue.id}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </S.Issues>
     </>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.github.com',
+  headers: {
+    Authorization: process.env.REACT_APP_GITHUB_AUTHORIZATION,
+  },
 });
 
 export default api;
