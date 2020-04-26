f07d739e29abf0d938d6f29ad6c221b394824b52
vitebo <vitebo@users.noreply.github.com>
2020-04-26 02:42:02 +0000

    deploy: 4ac42652fdf7e7a9feaaef4202a1f7e638481a38

---
+++ precache-manifest.5a8cb7f7fcedbd0ae23357f5e23247eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548ef9dd2a593cba3f171c3a08fb25e7",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "1e0418a305c9d1c73e2b",
+    "url": "/github-explorer/static/js/2.b27cf381.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.b27cf381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452b7eaeb13543411e7b",
+    "url": "/github-explorer/static/js/main.faa49d29.chunk.js"
+  },
+  {
+    "revision": "d23dd6c8660ea045fa68",
+    "url": "/github-explorer/static/js/runtime-main.efadcd73.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
